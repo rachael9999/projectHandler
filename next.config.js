07b3d715b888af20b23c0f6e053dad521e96b66f
account@acipiexport.com account@acipiexport.com <mrq@seas.upenn.edu>
2024-11-01 16:04:51 +0000

    memory

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -15,6 +17,22 @@ const nextConfig = {
         hostname: "images.pexels.com",
       },
     ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.cache = {
+        type: 'filesystem',
+        buildDependencies: {
+          config: [__filename],
+        },
+        cacheDirectory: path.resolve(__dirname, '.next/cache/webpack'),
+        store: 'pack',
+        compression: 'gzip',
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        profile: true,
+      };
+    }
+    return config;
   },
 };
 
